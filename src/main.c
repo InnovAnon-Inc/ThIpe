224a544086a8aaeb8237c6d31ba7559f26c99e6f
Default User <laurence.a.maddox@gmail.com>
2017-09-21 06:12:10 -0500

    added test app

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #define _POSIX_C_SOURCE 200112L
 #define __STDC_VERSION__ 200112L
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -23,6 +24,7 @@ static int thrio_cb (
 __attribute__ ((nothrow, warn_unused_result))
 int main (void) {
    error_check (thrio (
+      STDIN_FILENO, STDOUT_FILENO,
       (size_t) 10, (size_t) 3,
       (size_t) 8,  (size_t) 3,
       thrio_cb) != 0)
